19144e94e4c7f322fdce8879388a6c509f9eea38
Erick Matsen <ematsen@gmail.com>
2020-10-13 03:30:18 -0700

    templating + example

--- gpb/cli.py
+++ gpb/cli.py
@@ -1,51 +1,8 @@
 """Command line interface."""
 
-import pathlib
 import json
 import click
-import click_config_file
 import gpb.mb as mb
-
-def alignment_path_of_prefix(prefix):
-    return f"{prefix}.fasta"
-
-
-def json_provider(file_path, cmd_name):
-    """Enable loading of flags from a JSON file via click_config_file."""
-    if cmd_name:
-        with open(file_path) as config_data:
-            config_dict = json.load(config_data)
-            if cmd_name not in config_dict:
-                if "default" in config_dict:
-                    return config_dict["default"]
-                # else:
-                raise IOError(
-                    f"Could not find a '{cmd_name}' or 'default' section in '{file_path}'"
-                )
-            return config_dict[cmd_name]
-    # else:
-    return None
-
-
-def print_method_name_and_locals(method_name, local_variables):
-    """Print method name and local variables."""
-    if "ctx" in local_variables:
-        del local_variables["ctx"]
-    print(f"{method_name}{local_variables})")
-
-
-def restrict_dict_to_params(d_to_restrict, cmd):
-    """Restrict the given dictionary to the names of parameters for cmd."""
-    param_names = {param.name for param in cmd.params}
-    return {key: d_to_restrict[key] for key in d_to_restrict if key in param_names}
-
-
-def dry_run_option(command):
-    return click.option(
-        "--dry-run",
-        is_flag=True,
-        help="Only print paths and files to be made, rather than actually making them.",
-    )(command)
 
 
 # Entry point
@@ -54,14 +11,17 @@ def dry_run_option(command):
     invoke_without_command=True,
 )
 def cli():
-    pass
+    """Entrypoint."""
+    pass  # pylint: disable=unnecessary-pass
 
 
 @cli.command()
+@click.argument("settings_json", required=True, type=click.Path(exists=True))
 @click.argument("dest_path", required=True, type=click.Path(exists=False))
-def template(dest_path):
+def mbfile(settings_json, dest_path):
     """Generate a MrBayes input file."""
-    settings_dict = {"ngen":1234, "burnin":3}
+    with open(settings_json, "r") as file:
+        settings_dict = json.load(file)
     mb.generate_mb_input(settings_dict, dest_path)
 
 
--- gpb/mb.py
+++ gpb/mb.py
@@ -1,5 +1,6 @@
 """Template MrBayes input files."""
 
+import math
 from jinja2 import Environment, PackageLoader, StrictUndefined
 
 
@@ -8,9 +9,19 @@ def generate_mb_input(settings_dict, dest_path):
     Make MrBayes input file from a template.
     """
     env = Environment(
-        loader=PackageLoader('gpb', 'templates'),
-        undefined=StrictUndefined
+        loader=PackageLoader("gpb", "templates"), undefined=StrictUndefined
     )
-    template = env.get_template('basic.mb')
+    expand_mb_settings(settings_dict)
+    template = env.get_template(settings_dict["template"])
     with open(dest_path, "w") as file_obj:
         file_obj.write(template.render(**settings_dict) + "\n")
+
+
+def expand_mb_settings(settings_dict):
+    """
+    Expand settings in a settings dictionary to the full suite of things expected by MB.
+    """
+
+    if "burnin" not in settings_dict:
+        settings_dict["burnin"] = math.floor(
+            settings_dict["ngen"] * settings_dict["burnin_frac"])
