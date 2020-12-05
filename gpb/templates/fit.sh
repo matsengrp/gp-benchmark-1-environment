49b55a463fd5beb95ca8d522b5022f768dd85fc6
Erick Matsen <ematsen@gmail.com>
2020-12-05 04:55:58 -0800

    discarding singletons

--- gpb/templates/fit.sh
+++ gpb/templates/fit.sh
@@ -2,6 +2,8 @@ set -eux
 
 trap "rm -f mmap.dat" EXIT
 
-gpb fit --config {{config_path}} rerooted-topologies.noburnin.nwk {{output_prefix}}.fasta gp-sbn-parameters.csv
-gpb sa rerooted-topologies.noburnin.nwk sa-sbn-parameters.csv
+TREES=rerooted-topologies.noburnin.nonsingletons.nwk
+
+gpb fit --config {{config_path}} $TREES {{output_prefix}}.fasta gp-sbn-parameters.csv
+gpb sa $TREES sa-sbn-parameters.csv
 gpb compare gp-sbn-parameters.csv sa-sbn-parameters.csv comparison
--- gpb/templates/prep-gp.sh
+++ gpb/templates/prep-gp.sh
@@ -2,5 +2,6 @@ set -eu
 
 # rerooting on {{reroot_number}}, which is {{reroot_name}}
 awk '$1~/tree/ {print $NF}' {{output_prefix}}.t | nw_topology - | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin_samples}} > rerooted-topologies.noburnin.nwk
+sort rerooted-topologies.noburnin.nwk | uniq -c | sort -nr | awk '$1 > 1 {print $2}' > rerooted-topologies.noburnin.nonsingletons.nwk
 
 seqmagick convert {{nexus_alignment_path}} {{output_prefix}}.fasta
