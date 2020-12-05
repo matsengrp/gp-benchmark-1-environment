d7d7dbf556ca4429660ab795251cffa6ed36d787
Erick Matsen <ematsen@gmail.com>
2020-12-05 03:18:21 -0800

    more general templating

--- gpb/cli.py
+++ gpb/cli.py
@@ -2,7 +2,7 @@
 
 import json
 import click
-import gpb.mb as mb
+import gpb.templating as templating
 
 
 # Entry point
@@ -16,13 +16,14 @@ def cli():
 
 
 @cli.command()
+@click.argument("template_name", required=True)
 @click.argument("settings_json", required=True, type=click.Path(exists=True))
 @click.argument("dest_path", required=True, type=click.Path(exists=False))
-def mbfile(settings_json, dest_path):
-    """Generate a MrBayes input file."""
+def template(template_name, settings_json, dest_path):
+    """Generate a file using one of our templates and the settings."""
     with open(settings_json, "r") as file:
         settings_dict = json.load(file)
-    mb.generate_mb_input(settings_dict, dest_path)
+    templating.template_file(template_name, settings_dict, dest_path)
 
 
 if __name__ == "__main__":
