92bf928f4503d7171ae25cea00f086f0a19aaff7
Erick Matsen <ematsen@gmail.com>
2020-12-31 11:07:30 -0800

    compare.py updates

--- gpb/compare.py
+++ gpb/compare.py
@@ -9,12 +9,14 @@ sns.set_context("poster")
 
 def taxon_set_of_str_bitset(bitset):
     """Make the 1-indexed taxon set from a string bitset."""
-    return {index + 1 for index, value in enumerate(bitset) if value == '1'}
+    return {index + 1 for index, value in enumerate(bitset) if value == "1"}
 
 
 def indexed_pcsp_of_bitset_pcsp(pcsp_str):
     """Make a string representation of a PCSP described in 1-indexed form."""
-    [sister, focal, child1] = [taxon_set_of_str_bitset(bitset) for bitset in pcsp_str.split("|")]
+    [sister, focal, child1] = [
+        taxon_set_of_str_bitset(bitset) for bitset in pcsp_str.split("|")
+    ]
     return str(((sister, focal), (set(sorted(focal - child1)), child1)))
 
 
@@ -26,12 +28,15 @@ def indexed_gpcsp_of_bitset_gpcsp(gpcsp_str):
     return str(taxon_set_of_str_bitset(gpcsp_str))
 
 
-def parent_bitset_of_gpcsp(gpcsp_str):
+def parent_bitset_of_gpcsp(gpcsp_str, sort=False):
     """Given a PCSP string representation, extract the parent subsplit with no bar.
     Return empty string if there is no bar in the input (a rootsplit)."""
     if "|" in gpcsp_str:
         parent0, parent1, _ = gpcsp_str.split("|")
-        return "".join([parent0, parent1])
+        subsplits = [parent0, parent1]
+        if sort:
+            subsplits.sort()
+        return "".join(subsplits)
     # else:
     return ""
 
@@ -43,15 +48,25 @@ def rotate_subsplit(bitset_string):
     return bitset_string[half_len:] + bitset_string[:half_len]
 
 
-def sa_subsplit_probability_dict_of_csv(sa_subsplit_csv):
+def pretty_gpcsp_of_string(s):
+    """Insert vertical bars to make a PCSP bitset string easier to read."""
+    length = len(s)
+    assert length % 3 == 0
+    chunk_length = length // 3
+    return "|".join([s[i : i + chunk_length] for i in range(0, length, chunk_length)])
+
+
+def subsplit_probability_dict_of_csv(subsplit_csv):
     """Make a dictionary mapping from the subsplit (and its rotated version) to its
     probability given a subsplit probability CSV."""
-    sa_subsplit_df = pd.read_csv(sa_subsplit_csv, names=["subsplit", "probability"])
-    sa_subsplit_dict = {row["subsplit"]: row["probability"] for _, row in sa_subsplit_df.iterrows()}
-    for subsplit, probability in sa_subsplit_dict.copy().items():
-        sa_subsplit_dict[rotate_subsplit(subsplit)] = probability
-    sa_subsplit_dict[""] = 1.
-    return sa_subsplit_dict
+    subsplit_df = pd.read_csv(subsplit_csv, names=["subsplit", "probability"])
+    subsplit_dict = {
+        row["subsplit"]: row["probability"] for _, row in subsplit_df.iterrows()
+    }
+    for subsplit, probability in subsplit_dict.copy().items():
+        subsplit_dict[rotate_subsplit(subsplit)] = probability
+    subsplit_dict[""] = 1.0
+    return subsplit_dict
 
 
 def compare_parameters(gp_csv, sa_csv, sa_subsplit_csv, out_prefix):
@@ -62,7 +77,7 @@ def compare_parameters(gp_csv, sa_csv, sa_subsplit_csv, out_prefix):
     gp_df = pd.read_csv(gp_csv, names=["gpcsp", gp_name])
     sa_df = pd.read_csv(sa_csv, names=["gpcsp", sa_name])
 
-    sa_probabilities = sa_subsplit_probability_dict_of_csv(sa_subsplit_csv)
+    sa_probabilities = subsplit_probability_dict_of_csv(sa_subsplit_csv)
     sa_df["parent"] = sa_df["gpcsp"].apply(parent_bitset_of_gpcsp)
     sa_df["parent probability"] = sa_df["parent"].apply(sa_probabilities.get)
 
@@ -74,7 +89,7 @@ def compare_parameters(gp_csv, sa_csv, sa_subsplit_csv, out_prefix):
     df["delta"] = df[sa_name] - df[gp_name]
     df = df[[parent_probability_name, "delta", gp_name, sa_name, "gpcsp"]]
     # Drop rows where GP and SA both have them as 1.
-    df = df[(df[gp_name] < 1.) | (df[sa_name] < 1.)]
+    df = df[(df[gp_name] < 1.0) | (df[sa_name] < 1.0)]
     df.to_csv(out_prefix + ".csv", index=False)
 
     correlation = df.corr().at[gp_name, sa_name]
@@ -82,9 +97,11 @@ def compare_parameters(gp_csv, sa_csv, sa_subsplit_csv, out_prefix):
     corr_df = pd.DataFrame({"dataset": [out_prefix], "correlation": [correlation]})
     corr_df.to_csv("correlation.csv", index=False)
 
-    ax = sns.scatterplot(x=sa_name, y=gp_name, size=parent_probability_name, data=df, alpha=0.2)
+    ax = sns.scatterplot(
+        x=sa_name, y=gp_name, size=parent_probability_name, data=df, alpha=0.2
+    )
     ax.set_aspect(1)
     ax.set(ylim=ax.get_xlim())
     sns.despine()
-    plt.legend([],[], frameon=False)
+    plt.legend([], [], frameon=False)
     plt.savefig(out_prefix + ".svg", bbox_inches="tight")
