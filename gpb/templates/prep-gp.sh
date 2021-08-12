9d0d101d188bdd32797c1f1623426e04f743ec89
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-08-12 11:50:33 -0700

    small template fix

--- gpb/templates/prep-gp.sh
+++ gpb/templates/prep-gp.sh
@@ -1,7 +1,7 @@
 set -eu
 
 # rerooting on {{reroot_number}}, which is {{reroot_name}}
-awk '$1~/tree/ {print $NF}' {{output_prefix}}.t | nw_topology - | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin_samples}} > rerooted-topologies.noburnin.nwk
+awk '$1~/tree/ {print $NF}' {{output_prefix}}.t | nw_topology - | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin}} > rerooted-topologies.noburnin.nwk
 sort rerooted-topologies.noburnin.nwk | uniq -c | sort -nr | awk '$1 > 1 {print $2}' > rerooted-topologies.noburnin.nonsingletons.nwk
 
 seqmagick convert {{nexus_alignment_path}} {{output_prefix}}.fasta
