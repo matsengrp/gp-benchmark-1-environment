5db34756f2535221a884bfe5c8dee876367ceab4
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-11-19 17:14:13 -0800

    single tree plotting functions and templates

--- gpb/plot.py
+++ gpb/plot.py
@@ -52,3 +52,47 @@ def per_pcsp_likelihood_surfaces(per_pcsp_likelihood_surfaces_path, out_path):
         + p9.ylab("per pcsp marginal log likelihood")
     )
     plot.save(out_path)
+
+def per_pcsp_likelihood_surfaces_by_opt(nograd_surf_path, nograd_track_path, grad_surf_path, grad_track_path, out_path):
+
+    colnames = ['gpcsp', 'branch_length', 'llh']
+    
+    nograd_surf = pd.read_csv(nograd_surf_path, header = None, names = colnames)
+    nograd_track = pd.read_csv(nograd_track_path, header = None, names = colnames)
+    nograd_surf['opt'] = 'nograd'
+    nograd_track['opt'] = 'nograd'
+
+    grad_surf = pd.read_csv(grad_surf_path, header = None, names = colnames)
+    grad_track = pd.read_csv(grad_track_path, header = None, names = colnames)
+    grad_surf['opt'] = 'grad'
+    grad_track['opt'] = 'grad'
+
+    surf_df = nograd_surf.append(grad_surf, ignore_index=True, sort=False)
+    track_df = nograd_track.append(grad_track, ignore_index=True, sort=False)
+
+    track_df = track_df.drop_duplicates()
+    track_df['iter'] = pd.Categorical((track_df.groupby(['opt','gpcsp']).cumcount()) +1)
+
+    gpcsps = pd.unique(surf_df['gpcsp'])
+
+    plot_list = []
+
+    for gpcsp in gpcsps:
+        surface = surf_df[surf_df['gpcsp'] == gpcsp]
+        track = track_df[track_df['gpcsp'] == gpcsp]
+
+        plot = (
+            p9.ggplot(surface,
+                      p9.aes(x='branch_length', y = 'llh'))
+            + p9.geom_line(p9.aes(linetype= 'opt'))
+            + p9.geom_point(p9.aes(x='branch_length', y='llh',color='iter'),
+                            data = track)
+            + p9.xlab('branch_length')
+            + p9.ylab('per pcsp marginal log likelihood')
+            + p9.ggtitle(gpcsp)
+            + p9.theme(plot_title = p9.element_text(size = 6))
+        )
+        
+        plot_list.append(plot)
+
+    p9.save_as_pdf_pages(plot_list, filename = out_path)
