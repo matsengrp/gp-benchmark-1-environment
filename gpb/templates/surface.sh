f890838202d9ff0713938360521868d9e3f849d6
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2022-03-29 14:39:06 -0700

    script for llh surfaces

---
+++ gpb/templates/surface.sh
@@ -0,0 +1,11 @@
+set -eux
+
+trap "rm -f mmap.dat" EXIT
+
+TREES=rerooted-topologies.noburnin.nonsingletons.nwk
+gpb pcspsurface --config {{config_path}} $TREES {{output_prefix}}.fasta gp
+
+# reading in path for likelihood surfaces obtained after optimization
+for out_path in {{output_prefix}}.perpcsp.surface.svg {{output_prefix}}.perpcsp.surface.pdf; do
+	gpb pcspsurfaceplot gp.perpcsp_llh_surface.csv $out_path
+done
