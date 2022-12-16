467dde0e9b991bb278362d6990f6f259bf51c7b3
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2022-12-15 23:52:50 -0800

    changes to templates to reflect organization changes in gp-benchmark-1 repo

--- gpb/templates/prep-gp-benchmark.sh
+++ gpb/templates/prep-gp-benchmark.sh
@@ -3,9 +3,11 @@ set -eu
 # rerooting on {{reroot_number}}, which is {{reroot_name}}
 # doing this on the from the MB runs with a uniform prior to load into GP
 # and also on the trees from MB  with an exponential prior to assess VBPI fit
-awk '$1~/tree/ {print $NF}' {{output_prefix}}.unif.t | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin_samples}} > rerooted-topologies.unif.noburnin.withbranchlengths.nwk
+UNIFPATH=../MrBayes/unif
+EXPPATH=../MrBayes/exp
+awk '$1~/tree/ {print $NF}' $UNIFPATH/{{output_prefix}}.t | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin_samples}} > rerooted-topologies.unif.noburnin.withbranchlengths.nwk
 
-awk '$1~/tree/ {print $NF}' {{output_prefix}}.exp.t | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin_samples}} > rerooted-topologies.exp.noburnin.withbranchlengths.nwk
+awk '$1~/tree/ {print $NF}' $EXPPATH/{{output_prefix}}.t | nw_reroot - {{reroot_number}} | nw_order - | tail -n +{{burnin_samples}} > rerooted-topologies.exp.noburnin.withbranchlengths.nwk
 
 # outputting unique topologies to assess how much faster it is build engine
 nw_topology rerooted-topologies.unif.noburnin.withbranchlengths.nwk | sort | uniq > rerooted-topologies.unif.noburnin.uniq.nwk
